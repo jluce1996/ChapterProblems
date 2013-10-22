05c56bae97cce88d3bc0491e78cad6d73da694f7
jluce1996 <jluce1996@gmail.com>
2013-10-22 08:16:19 -0400

    finished 4

--- ChapterProblems/Problem 4 pg. 195/Program.cs
+++ ChapterProblems/Problem 4 pg. 195/Program.cs
@@ -9,6 +9,30 @@ namespace Problem_4_pg._195
     {
         static void Main(string[] args)
         {
+            int[] areacode = new int[6] { 262, 414, 608, 715, 815, 920 };
+            Double[] rate = new double[6] { .07, .10, .05, .16, .24, .14 };
+            double price = 0;
+            string input;
+            string inputminutes;
+            
+            Console.WriteLine("Enter your area code");
+            input = Console.ReadLine();
+            Console.WriteLine("Enter your number of minutes");
+            inputminutes = Console.ReadLine();
+
+            for (int i = 0; i < areacode.Length; i++)
+            {
+                if (input == areacode[i].ToString())
+                {
+                    price = rate[i] * Convert.ToInt32(inputminutes);
+                    Console.WriteLine("Your cost is" +     price.ToString("C"));
+                    break;
+                }
+                else if (i == 5)
+                {
+                    Console.WriteLine("Entered an invalid area code.");
+                }
+            }
         }
     }
 }
