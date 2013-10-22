cd1c9dffa1065da9c66c702c60233f3c66db2d6e
jluce1996 <jluce1996@gmail.com>
2013-10-22 08:39:29 -0400

    started 7. finished 3

---
+++ ChapterProblems/Problem 3 pg.244/Monogram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_3_pg._244
+{
+    class Monogram
+    {
+        static void Main()
+        {
+            string myfirst;
+            string friendfirst;
+            string mymiddle;
+            string friendmiddle;
+            string mylast;
+            string friendlast;
+            DisplayMonogram(myfirst, mymiddle, mylast);
+            
+        }
+        public static void DisplayMonogram(string first, string middle, string last)
+        {
+            Console.WriteLine("** {0}. {1}. {2}. **", first, middle, last);
+
+
+        }
+        
+    }
+}
---
+++ ChapterProblems/Problem 7 pg. 195/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_7_pg._195
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carprice; 
+            double price; 
+            double commissions = 0;
+            Console.Write("What is the price of the car?:");
+            carprice = Console.ReadLine();
+            price = Convert.ToDouble(carprice);
+             
+
+            if(price < 15000)
+            {
+                commissions = price * .05;
+                Console.WriteLine("Your commissions are" + commissions.ToString("C"));
+            }
+            if (price > 15000 && price <= 24000)
+            {
+                commissions = price * .07;
+                Console.WriteLine("Your commissions are" + commissions.ToString("C"));
+            }
+            if (price > 24000)
+            {
+                commissions = price * .10;
+                Console.WriteLine("Your commissions are" + commissions.ToString("C"));
+            }
+        }
+    }
+}
